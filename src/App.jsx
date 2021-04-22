2e878f38adea8b5649ab520aa9744410a77d69ef
Abdel <abdelmawla.souat@gmail.Com>
2021-04-23 00:49:03 +0200

    Add Score Panel Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,29 @@
  * Email: abdelmawla.souat@gmail.com
  * -----
  * Created at: 2021-04-22 6:51:44 pm
- * Last Modified: 2021-04-22 7:04:03 pm
+ * Last Modified: 2021-04-23 12:28:22 am
  * -----
  * Copyright (c) 2021 Yuei
  */
 
 import React, { Component } from 'react';
+import ScorePanel from './components/ScorePanel';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      score: 5,
+    };
+  }
+
   render() {
-    return <h1>Hello World</h1>;
+    const { score } = this.state;
+    return (
+      <main>
+        <ScorePanel score={score} />
+      </main>
+    );
   }
 }
 
--- src/components/ScorePanel.jsx
+++ src/components/ScorePanel.jsx
@@ -4,17 +4,40 @@
  * Email: abdelmawla.souat@gmail.com
  * -----
  * Created at: 2021-04-22 7:22:29 pm
- * Last Modified: 2021-04-22 7:23:38 pm
+ * Last Modified: 2021-04-23 12:45:12 am
  * -----
  * Copyright (c) 2021 Yuei
  */
 
 import React, { Component } from 'react';
 
+import PropTypes from 'prop-types';
+import styles from './ScorePanel.module.scss';
+import logo from '../assets/images/logo-bonus.svg';
+
 class ScorePanel extends Component {
   render() {
-    return <span>Yosh</span>;
+    const { score } = this.props;
+
+    return (
+      <div className={styles.scorePanel}>
+        <img
+          className={styles.logo}
+          src={logo}
+          alt="Rock, Paper, Scissors, Lizard, Spock"
+        />
+
+        <div className={styles.scoreContainer}>
+          <span>SCORE</span>
+          <span className={styles.score}>{score}</span>
+        </div>
+      </div>
+    );
   }
 }
+
+ScorePanel.propTypes = {
+  score: PropTypes.number.isRequired,
+};
 
 export default ScorePanel;
